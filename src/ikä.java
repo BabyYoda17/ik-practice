87aaabb32a44a7bed890ff65c0c0fedae547d1ff
BabyYoda17 <samuel22006@student.hamk.fi>
2023-10-09 18:07:27 +0300

    Initial commit

---
+++ src/ikä.java
@@ -0,0 +1,53 @@
+
+public class ikä {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int ika = 60;
+	if (ika > 0 && ika < 18)
+	{
+		System.out.println("OLet alaikäinen");
+	if (ika >= 15) {
+		System.out.println("Saat ajaa mopoa");
+	if (ika >= 16) {
+		System.out.println("Saat ajaa kevaria");
+	}
+	}
+	}
+	else if (ika >= 65)
+	{
+		System.out.println("Olet eläkeläinen");
+		System.out.println("Hyviä eläkepäiviä");
+	}
+	if (ika >= 58)
+	{
+		System.out.println("Voit mennä varhaiseläkkeelle");
+	}
+	else
+	{
+		System.out.println("Olet aikuinen");
+	}
+	if (ika >= 18)
+	{
+		System.out.println("Saat ajaa autos");
+	}
+	
+	
+	if(ika % 10 == 0)
+	{ 
+		System.out.println("Onnea tasakymmenvuosista");
+	}
+	if (ika == 100)
+	{
+		System.out.println("Onnea");
+		System.out.println("100");
+		System.out.println("vuotiaalle");
+	}
+	
+	
+	
+	
+	}
+
+}
